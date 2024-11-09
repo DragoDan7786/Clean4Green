c02f7f88b1e7bc53e8a0dfd23e1bb6d77222ff7c
DragoDan <grimlock7786@gmail.com>
2024-11-09 17:00:43 -0500

    Edited Primary Key wording

--- General/Scripts/Table Creation.sql
+++ General/Scripts/Table Creation.sql
@@ -1,11 +1,10 @@
 CREATE TABLE user (
-    userID INTEGER,  -- Define userID as an INTEGER (no PRIMARY KEY here)
+    userID INTEGER PRIMARY KEY AUTOINCREMENT
     userName VARCHAR(250) NOT NULL UNIQUE,
     pWord VARCHAR(250) NOT NULL,
     firstName VARCHAR(250) NOT NULL,
     lastName VARCHAR(250) NOT NULL,
     email VARCHAR(250) NOT NULL,
-    CONSTRAINT user_pk PRIMARY KEY(userID)  -- Define a named primary key constraint
 );
 CREATE TABLE Submissions (
     SubID INTEGER PRIMARY KEY AUTOINCREMENT
